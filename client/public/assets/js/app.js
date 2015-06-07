56086c51471c32e0217be5b557002b214b3fdc54
Matt McFadyen <mmcfadyen91@gmail.com>
2015-06-06 21:44:27 -0400

    :beers::beers:#22 added filter to format date to today || yesterday || last week || awhile ago

--- client/public/assets/js/app.js
+++ client/public/assets/js/app.js
@@ -50,14 +50,47 @@ app.config(["$routeProvider",
 	};
 });
 
-angular.module("filters", []).filter("shortFullName", function() {
-	return function(fullName) {
-		console.log(fullName);
-		if (fullName) {
-			var name = fullName.split(" ")
-			return name[0] + " " + name[1][0] + ".";
-		} else {
-			return "";
+angular.module("filters", [])
+	.filter("shortFullName", function() {
+		return function(fullName) {
+			if (fullName) {
+				var name = fullName.split(" ")
+				return name[0] + " " + name[1][0] + ".";
+			} else {
+				return "";
+			}
+		};
+	})
+	.filter("smartDate", function() {
+		var today = new Date(),
+			day = today.getDate(),
+			month = today.getMonth();
+
+		return function(date) {
+			if (date){
+				var dayOfWeek = ["Monday", "Tuesday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+				var date = new Date(date),
+					dateDay = date.getDate(),
+					dateMonth = date.getMonth(),
+					time = date.toLocaleTimeString().split(":");
+					time = time[0] + ":" + time[1] + time[2].substr(time[2].length-2, 2).toLowerCase();
+					console.log(day, dateDay);
+				if (day == dateDay && month == dateMonth) {
+					return "Today " + time;
+				} else if (day - dateDay === 1) {
+					return "Yesterday " + time;
+				} else if ((day - dateDay < 7 && dateMonth === month) || dateMonth - month === 1 && (day + 30) - dateDay < 7 ) {
+					return dayOfWeek[date.getDay()] +" " + time;
+				} else if(day - dateDay >= 7 && day - dateDay < 14 
+					 || (month - dateMonth === 1) && (day - (dateDay - 30) >= 7  && day - (dateDay - 30) < 14  ) ){
+					return "Last Week";
+				}
+				else {
+					return "Awhile ago"
+				}
+			} else {
+				return "";
+			}
+
 		}
-	};
-});
+	})
