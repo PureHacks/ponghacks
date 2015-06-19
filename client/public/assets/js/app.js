4e7e480bd0900466f247a583676b54602cfbac00
Matt McFadyen <mmcfadyen91@gmail.com>
2015-06-19 12:20:14 -0400

    tv nav hotfix

--- client/public/assets/js/app.js
+++ client/public/assets/js/app.js
@@ -17,6 +17,10 @@ app.config(["$routeProvider",
 				templateUrl: "partials/dashboard.html",
 				controller: "dashboardCtrl"
 			})
+			.when("/tv", {
+				templateUrl: "partials/dashboard.html",
+				controller: "dashboardCtrl"
+			})
 			.when("/player-stats", {
 				templateUrl: "partials/playerStats.html",
 				controller: "playerStatsCtrl"
